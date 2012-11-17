50341c079614f573eebd221ec77203d78c532475
Mark Voros <markvoros@gmail.com>
2012-11-17 16:57:04 -0500

    Added the design for master page
    This is the design for the site moving forward. If you have any
    suggestions or comments, let me know. I also commented out the
    LoadCharacter class, because it had compile errors.

--- App_Code/Database Loaders/LoadCharacters.cs
+++ App_Code/Database Loaders/LoadCharacters.cs
@@ -1,40 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Xml.Linq;
-using System.Data.SqlClient;
-using System.Configuration;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Xml.Linq;
+//using System.Data.SqlClient;
+//using System.Configuration;
 
-/// <summary>
-/// Summary description for LoadCharacters
-/// </summary>
-public class LoadCharacters
-{
-    public static void LoadCharactersIntoDatabase(string path, HttpResponse response)
-    {
-        XDocument charactersXML = XDocument.Load(path + "\\XML Files\\Characters.xml");
-        IEnumerable<Character> characters =
-            from character in charactersXML.Descendants("character")
-            select new Character(character);
+///// <summary>
+///// Summary description for LoadCharacters
+///// </summary>
+//public class LoadCharacters
+//{
+//    public static void LoadCharactersIntoDatabase(string path, HttpResponse response)
+//    {
+//        XDocument charactersXML = XDocument.Load(path + "\\XML Files\\Characters.xml");
+//        IEnumerable<Character> characters =
+//            from character in charactersXML.Descendants("character")
+//            select new Character(character);
 
-		SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["connStr_LeagueOfLegends"].ConnectionString);
-		conn.Open();
-		SqlCommand cmd = new SqlCommand();
-		cmd.Connection = conn;
+//        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["connStr_LeagueOfLegends"].ConnectionString);
+//        conn.Open();
+//        SqlCommand cmd = new SqlCommand();
+//        cmd.Connection = conn;
 
-        foreach (Character character in characters)
-        {
-			string sql = "INSERT INTO [Characters] ([id], [name]) VALUES(";
-			sql += "'" + character.getID().ToString() + "',";
-			sql += "'" + character.getName() + "'";
-			sql += ")";
-			cmd.CommandText = sql;
-			bool worked = Convert.ToBoolean(cmd.ExecuteNonQuery());
-			if (!worked)
-				response.Write(character.ToString() + "   -   Not written <br/>");
-        }
+//        foreach (Character character in characters)
+//        {
+//            string sql = "INSERT INTO [Characters] ([id], [name]) VALUES(";
+//            sql += "'" + character.getID().ToString() + "',";
+//            sql += "'" + character.getName() + "'";
+//            sql += ")";
+//            cmd.CommandText = sql;
+//            bool worked = Convert.ToBoolean(cmd.ExecuteNonQuery());
+//            if (!worked)
+//                response.Write(character.ToString() + "   -   Not written <br/>");
+//        }
 
-		conn.Close();
-    }
-}
+//        conn.Close();
+//    }
+//}
