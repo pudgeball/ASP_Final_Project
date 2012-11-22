128d435d5d53aa8b6a343bfc72bbad49f8486eb8
Nick McGuire <nick@nickmcguire.com>
2012-11-22 16:06:46 -0500

    Updated New Build
    Updated new build to use the web service

--- NewBuild.aspx.cs
+++ NewBuild.aspx.cs
@@ -11,59 +11,31 @@ using LeagueOfLegends;
 
 public partial class NewBuild : System.Web.UI.Page
 {
-	private int index;
+	localhost.LeagueOfLegendsWebService webService;
 
     protected void Page_Load(object sender, EventArgs e)
     {
-		localhost.LeagueOfLegendsWebService leagueService = new localhost.LeagueOfLegendsWebService();
-		List<localhost.Character> characters = leagueService.GetCharacters().ToList<localhost.Character>();
+		webService = new localhost.LeagueOfLegendsWebService();
+		List<localhost.Character> characters = webService.GetCharacters().ToList<localhost.Character>();
 
         dropCharacter.SelectedIndexChanged += new EventHandler(characterSelect_SelectedIndexChanged);
-
-		if (Page.IsPostBack)
-		{
-			lblLevelOne.Visible = true;
-			dropLevelOne.Visible = true;
-		}
-		else
+		if (!Page.IsPostBack)
 		{
 			dropCharacter.DataSource = characters;
 			dropCharacter.DataTextField = "Name";
 			dropCharacter.DataValueField = "ID";
 			dropCharacter.DataBind();
+
+			dropLevelOne.DataSource = webService.GetAbilities(0);
+			dropLevelOne.DataTextField = "Name";
+			dropLevelOne.DataValueField = "Name";
+			dropLevelOne.DataBind();
 		}
     }
 
     void characterSelect_SelectedIndexChanged(object sender, EventArgs e)
     {
-		DropDownList listBox = (DropDownList)sender;
-		string connStr = ConfigurationManager.ConnectionStrings["connStr_LeagueOfLegends"].ConnectionString;
-		SqlConnection conn = new SqlConnection(connStr);
-		conn.Open();
-		SqlCommand cmd = new SqlCommand();
-		cmd.Connection = conn;
-		string sql = "SELECT [Abilities].[name], [Abilities].[description] FROM [Abilities] INNER JOIN [CharactersAbilities] ON [Abilities].[name] = [CharactersAbilities].[abilityName] INNER JOIN [Characters] ON [CharactersAbilities].[characterId] = [Characters].[id] WHERE [Characters].[id] = " + listBox.Items[listBox.SelectedIndex].Value;
-		cmd.CommandText = sql;
-		SqlDataReader dr = cmd.ExecuteReader();
 
-		Response.Write(sql);
-		if (dr.HasRows)
-		{
-			while (dr.Read())
-			{
-				string Name = dr["name"].ToString();
-				string Description = dr["description"].ToString();
-				//abilities.Add(new Ability(Name, Description));
-			}
-		}
-		else
-		{
-			Response.Write("No Rows");
-		}
-		dr.Close();
-		conn.Close();
-
-		//Response.Write(abilities.Count);
 		localhost.LeagueOfLegendsWebService leagueService = new localhost.LeagueOfLegendsWebService();
 		int id = Convert.ToInt32(listBox.Items[listBox.SelectedIndex].Value);
 		dropLevelOne.DataSource = leagueService.GetAbilities(id);
