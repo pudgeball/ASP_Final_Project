b499998350002081ca9311d55ce691897940746e
Nick McGuire <pudgeball@me.com>
2012-11-23 12:57:19 -0500

    this
    Added the table to the design mode instead of at runtime.
    It will now be able to properly create builds, I just need to do
    validation and creation of those methods.

--- NewBuild.aspx
+++ NewBuild.aspx
@@ -1,6 +1,14 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="NewBuild.aspx.cs" Inherits="NewBuild" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    <style type="text/css">
+        .style1
+        {
+            width: 100%;
+            border-style: solid;
+            border-width: 1px;
+        }
+    </style>
     </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="pageContent" Runat="Server">
     <asp:Label ID="Label1" runat="server" Text="Build Name: "></asp:Label>
@@ -14,6 +22,169 @@
     <br />
 &nbsp;<br />
     <asp:PlaceHolder ID="placeholderAbilityTable" runat="server"></asp:PlaceHolder>
+    <br />
+    <br />
+    <table class="style1">
+        <tr>
+            <td>
+                <asp:Label ID="Label8" runat="server" Text="Level"></asp:Label>
+            </td>
+            <td>
+                <asp:Label ID="Label10" runat="server" Text="1"></asp:Label>
+            </td>
+            <td>
+                <asp:Label ID="Label11" runat="server" Text="2"></asp:Label>
+            </td>
+            <td>
+                <asp:Label ID="Label12" runat="server" Text="3"></asp:Label>
+            </td>
+            <td>
+                <asp:Label ID="Label13" runat="server" Text="4"></asp:Label>
+            </td>
+            <td>
+                <asp:Label ID="Label14" runat="server" Text="5"></asp:Label>
+            </td>
+            <td>
+                <asp:Label ID="Label15" runat="server" Text="6"></asp:Label>
+            </td>
+            <td>
+                <asp:Label ID="Label16" runat="server" Text="7"></asp:Label>
+            </td>
+            <td>
+                <asp:Label ID="Label17" runat="server" Text="8"></asp:Label>
+            </td>
+            <td>
+                <asp:Label ID="Label18" runat="server" Text="9"></asp:Label>
+            </td>
+            <td>
+                <asp:Label ID="Label19" runat="server" Text="10"></asp:Label>
+            </td>
+            <td>
+                <asp:Label ID="Label20" runat="server" Text="11"></asp:Label>
+            </td>
+            <td>
+                <asp:Label ID="Label21" runat="server" Text="12"></asp:Label>
+            </td>
+            <td>
+                <asp:Label ID="Label22" runat="server" Text="13"></asp:Label>
+            </td>
+            <td>
+                <asp:Label ID="Label23" runat="server" Text="14"></asp:Label>
+            </td>
+            <td>
+                <asp:Label ID="Label24" runat="server" Text="15"></asp:Label>
+            </td>
+            <td>
+                <asp:Label ID="Label25" runat="server" Text="16"></asp:Label>
+            </td>
+            <td>
+                <asp:Label ID="Label26" runat="server" Text="17"></asp:Label>
+            </td>
+            <td>
+                <asp:Label ID="Label9" runat="server" Text="18"></asp:Label>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Label ID="lblAbility1" runat="server" Text="Label"></asp:Label>
+            </td>
+            <td colspan="18">
+                <asp:Label ID="Label27" runat="server" Text="Passive"></asp:Label>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Label ID="lblAbility2" runat="server" Text="Label"></asp:Label>
+            </td>
+            <td rowspan="4">
+                <asp:RadioButtonList ID="radioListLevel1" runat="server">
+                </asp:RadioButtonList>
+            </td>
+            <td rowspan="4">
+                <asp:RadioButtonList ID="radioListLevel2" runat="server">
+                </asp:RadioButtonList>
+            </td>
+            <td rowspan="4">
+                <asp:RadioButtonList ID="radioListLevel3" runat="server">
+                </asp:RadioButtonList>
+            </td>
+            <td rowspan="4">
+                <asp:RadioButtonList ID="radioListLevel4" runat="server">
+                </asp:RadioButtonList>
+            </td>
+            <td rowspan="4">
+                <asp:RadioButtonList ID="radioListLevel5" runat="server">
+                </asp:RadioButtonList>
+            </td>
+            <td rowspan="4">
+                <asp:RadioButtonList ID="radioListLevel6" runat="server">
+                </asp:RadioButtonList>
+            </td>
+            <td rowspan="4">
+                <asp:RadioButtonList ID="radioListLevel7" runat="server">
+                </asp:RadioButtonList>
+            </td>
+            <td rowspan="4">
+                <asp:RadioButtonList ID="radioListLevel8" runat="server">
+                </asp:RadioButtonList>
+            </td>
+            <td rowspan="4">
+                <asp:RadioButtonList ID="radioListLevel9" runat="server">
+                </asp:RadioButtonList>
+            </td>
+            <td rowspan="4">
+                <asp:RadioButtonList ID="radioListLevel10" runat="server">
+                </asp:RadioButtonList>
+            </td>
+            <td rowspan="4">
+                <asp:RadioButtonList ID="radioListLevel11" runat="server">
+                </asp:RadioButtonList>
+            </td>
+            <td rowspan="4">
+                <asp:RadioButtonList ID="radioListLevel12" runat="server">
+                </asp:RadioButtonList>
+            </td>
+            <td rowspan="4">
+                <asp:RadioButtonList ID="radioListLevel13" runat="server">
+                </asp:RadioButtonList>
+            </td>
+            <td rowspan="4">
+                <asp:RadioButtonList ID="radioListLevel14" runat="server">
+                </asp:RadioButtonList>
+            </td>
+            <td rowspan="4">
+                <asp:RadioButtonList ID="radioListLevel15" runat="server">
+                </asp:RadioButtonList>
+            </td>
+            <td rowspan="4">
+                <asp:RadioButtonList ID="radioListLevel16" runat="server">
+                </asp:RadioButtonList>
+            </td>
+            <td rowspan="4">
+                <asp:RadioButtonList ID="radioListLevel17" runat="server">
+                </asp:RadioButtonList>
+            </td>
+            <td rowspan="4">
+                <asp:RadioButtonList ID="radioListLevel18" runat="server">
+                </asp:RadioButtonList>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Label ID="lblAbility3" runat="server" Text="Label"></asp:Label>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Label ID="lblAbility4" runat="server" Text="Label"></asp:Label>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Label ID="lblAbility5" runat="server" Text="Label"></asp:Label>
+            </td>
+        </tr>
+    </table>
     <br />
     <br />
     <br />
--- NewBuild.aspx.cs
+++ NewBuild.aspx.cs
@@ -12,35 +12,30 @@ using LeagueOfLegends;
 public partial class NewBuild : System.Web.UI.Page
 {
 	localhost.LeagueOfLegendsWebService webService = new localhost.LeagueOfLegendsWebService();
-	List<localhost.Item> buildItems = new List<localhost.Item>();
-	List<localhost.Item> allItems = new List<localhost.Item>();
-	List<localhost.Ability> abilties = new List<localhost.Ability>();
+	List<localhost.Item> buildItems;
+	List<localhost.Item> allItems;
+	List<localhost.Ability> abilties;
+	List<RadioButtonList> radioButtonList;
 
     protected void Page_Load(object sender, EventArgs e)
     {
 		if (!Page.IsPostBack)
 		{
 			allItems = webService.GetItems().ToList<localhost.Item>();
+			buildItems = new List<localhost.Item>();
 
 			dropCharacter.DataSource = webService.GetCharacters();
 			dropCharacter.DataTextField = "Name";
 			dropCharacter.DataValueField = "ID";
 			dropCharacter.DataBind();
 
-			int id = Convert.ToInt32(dropCharacter.Items[dropCharacter.SelectedIndex].Value);
-
-			abilties = webService.GetAbilitiesForCharacter(id).ToList<localhost.Ability>();
-
-			AddTableToPage(true);
-			bindListBoxs();
-
+			Session["CharacterID"] = Convert.ToInt32(dropCharacter.Items[dropCharacter.SelectedIndex].Value);
 			Session["allItems"] = allItems;
 			Session["buildItems"] = buildItems;
-			Session["abilties"] = abilties;
-		}
 
+			UpdateUI();
+		}
 		dropCharacter.SelectedIndexChanged += new EventHandler(characterSelect_SelectedIndexChanged);
-		
     }
 
     void characterSelect_SelectedIndexChanged(object sender, EventArgs e)
@@ -48,14 +43,56 @@ public partial class NewBuild : System.Web.UI.Page
 		DropDownList listBox = (DropDownList)sender;
 
 		int id = Convert.ToInt32(listBox.Items[listBox.SelectedIndex].Value);
-		abilties = webService.GetAbilitiesForCharacter(id).ToList<localhost.Ability>();
+		
 		Session["CharacterID"] = id;
 
-		AddTableToPage(true);
+		UpdateUI();
     }
 
-	private void bindListBoxs()
+	private void UpdateUI()
 	{
+		int id = Convert.ToInt32(Session["CharacterID"]);
+		abilties = webService.GetAbilitiesForCharacter(id).ToList<localhost.Ability>();
+
+		radioButtonList = new List<RadioButtonList>();
+		radioButtonList.Add(radioListLevel1);
+		radioButtonList.Add(radioListLevel2);
+		radioButtonList.Add(radioListLevel3);
+		radioButtonList.Add(radioListLevel4);
+		radioButtonList.Add(radioListLevel5);
+		radioButtonList.Add(radioListLevel6);
+		radioButtonList.Add(radioListLevel7);
+		radioButtonList.Add(radioListLevel8);
+		radioButtonList.Add(radioListLevel9);
+		radioButtonList.Add(radioListLevel10);
+		radioButtonList.Add(radioListLevel11);
+		radioButtonList.Add(radioListLevel12);
+		radioButtonList.Add(radioListLevel13);
+		radioButtonList.Add(radioListLevel14);
+		radioButtonList.Add(radioListLevel15);
+		radioButtonList.Add(radioListLevel16);
+		radioButtonList.Add(radioListLevel17);
+		radioButtonList.Add(radioListLevel18);
+
+		lblAbility1.Text = abilties[0].Name.ToString();
+		lblAbility2.Text = abilties[1].Name.ToString();
+		lblAbility3.Text = abilties[2].Name.ToString();
+		lblAbility4.Text = abilties[3].Name.ToString();
+		lblAbility5.Text = abilties[4].Name.ToString();
+
+
+		abilties.Remove(abilties[0]);
+
+		foreach (RadioButtonList buttonList in radioButtonList)
+		{
+			buttonList.Items.Clear();
+			buttonList.DataSource = abilties;
+			buttonList.DataTextField = "Name";
+			buttonList.DataTextFormatString = "&nbsp";
+			buttonList.DataValueField = "Name";
+			buttonList.DataBind();
+		}
+
 		listAllItems.DataSource = allItems;
 		listAllItems.DataTextField = "Name";
 		listAllItems.DataBind();
@@ -75,8 +112,7 @@ public partial class NewBuild : System.Web.UI.Page
 
 		cmdAdd.Enabled = false;
 
-		AddTableToPage(false);
-		bindListBoxs();
+		UpdateUI();
 	}
 
 	protected void cmdRemove_Click(object sender, EventArgs e)
@@ -89,141 +125,42 @@ public partial class NewBuild : System.Web.UI.Page
 
 		cmdRemove.Enabled = false;
 
-		AddTableToPage(false);
-		bindListBoxs();
+		UpdateUI();
 	}
 
 	protected void cmdCreate_Click(object sender, EventArgs e)
 	{
-		Table abilityTable = (Table)Session["abilityTable"];
-		List<string> selectedAbilities = new List<string>();
-
-		for (int i = 2; i < abilityTable.Rows.Count; i++)
-		{
-			for (int j = 1; j < abilityTable.Rows[i].Cells.Count; j++)
-			{
-				if (abilityTable.Rows[i].Cells[j].Controls.Count > 0)
-				{
-					RadioButton radioButton = (RadioButton)abilityTable.Rows[i].Cells[j].Controls[0];
-					if (radioButton.Checked == true)
-					{
-						Response.Write("zomg");
-						selectedAbilities.Add(radioButton.ToolTip);
-					}
-				}
-			}
-		}
-
-		foreach (string s in selectedAbilities)
-		{
-			Response.Write(s + "<br/>");
-		}
-		AddTableToPage(false);
-	}
-
-	private Table CreateAbilityTable()
-	{
-		Table abilityTable = new Table();
-		abilityTable.BorderWidth = 1;
-		int id = Convert.ToInt32(Session["CharacterID"]);
-		Response.Write("Current ID " + id);
-		abilties = webService.GetAbilitiesForCharacter(id).ToList<localhost.Ability>();
-		
-		for (int i = 0; i < 6; i++)
-		{
-			TableRow row = new TableRow();
-			for (int j = 0; j < 19; j++)
-			{
-				if (i == 0)
-				{
-					TableHeaderCell cell = new TableHeaderCell();
-					cell.BorderWidth = 1;
-					cell.Text = (j > 0) ? j.ToString() : "Level";
-					row.Cells.Add(cell);
-				}
-				else
-				{
-					TableCell cell = new TableCell();
-					cell.BorderWidth = 1;
-					if (i == 1)
-					{
-						if (j == 0)
-						{
-							cell.Text = abilties[i - 1].Name;
-						}
-						else if (j == 1)
-						{
-							cell.ColumnSpan = 18;
-							cell.Text = "Passive";
-						}
-						else
-						{
-							cell = null;
-						}
-					}
-					else
-					{
-						if (j > 0)
-						{
-							RadioButton radioButton = new RadioButton();
-							radioButton.GroupName = "Level" + j.ToString();
-							radioButton.ToolTip = abilties[i - 1].Name;
-							radioButton.AutoPostBack = true;
-							radioButton.CheckedChanged += new EventHandler(radioButton_CheckedChanged);
-							cell.Controls.Add(radioButton);
-						}
-						else
-						{
-
-							cell.Text = abilties[i - 1].Name;
-						}
-					}
-
-					if (cell != null)
-					{
-						row.Cells.Add(cell);
-					}
-				}
-			}
-			abilityTable.Rows.Add(row);
-		}
-
-		return abilityTable;
-	}
-
-	void radioButton_CheckedChanged(object sender, EventArgs e)
-	{
-		RadioButton radioButton = (RadioButton)sender;
-		Response.Write(radioButton.GroupName + " - " + radioButton.ToolTip);
-		AddTableToPage(false);
-	}
-
-	private void AddTableToPage(bool NewCharacter)
-	{
-		Table abilityTable;
-
-		if (NewCharacter)
-		{
-			abilityTable = CreateAbilityTable();
-			
-			Session["abilityTable"] = abilityTable;
-		}
-		else
-		{
-			abilityTable = (Table)Session["abilityTable"];
-		}
-
-		placeholderAbilityTable.Controls.Add(abilityTable);
+		output();
 	}
 
 	protected void listAllItems_SelectedIndexChanged(object sender, EventArgs e)
 	{
-		AddTableToPage(false);
 		cmdAdd.Enabled = true;
 	}
 	protected void listBuildItems_SelectedIndexChanged(object sender, EventArgs e)
 	{
-		AddTableToPage(false);
 		cmdRemove.Enabled = true;
+	}
+
+	private void output()
+	{
+		Response.Write(radioListLevel1.SelectedItem.Value);
+		Response.Write(radioListLevel2.SelectedItem.Value);
+		Response.Write(radioListLevel3.SelectedItem.Value);
+		Response.Write(radioListLevel4.SelectedItem.Value);
+		Response.Write(radioListLevel5.SelectedItem.Value);
+		Response.Write(radioListLevel6.SelectedItem.Value);
+		Response.Write(radioListLevel7.SelectedItem.Value);
+		Response.Write(radioListLevel8.SelectedItem.Value);
+		Response.Write(radioListLevel9.SelectedItem.Value);
+		Response.Write(radioListLevel10.SelectedItem.Value);
+		Response.Write(radioListLevel11.SelectedItem.Value);
+		Response.Write(radioListLevel12.SelectedItem.Value);
+		Response.Write(radioListLevel13.SelectedItem.Value);
+		Response.Write(radioListLevel14.SelectedItem.Value);
+		Response.Write(radioListLevel15.SelectedItem.Value);
+		Response.Write(radioListLevel16.SelectedItem.Value);
+		Response.Write(radioListLevel17.SelectedItem.Value);
+		Response.Write(radioListLevel18.SelectedItem.Value);
 	}
 }
