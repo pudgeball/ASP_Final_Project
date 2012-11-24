4213a121f74f918eb05c43cca638ede5a34ad7fd
Mark Voros <markvoros@gmail.com>
2012-11-23 19:26:28 -0500

    TierList page added, portraits added
    Created the TierList page (so far empty), and added images for every
    character to the Images folder.

---
+++ TierList.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="TierList.aspx.cs" Inherits="TierList" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="pageContent" Runat="Server">
+    <h1>Tier List</h1>
+    <asp:PlaceHolder ID="tierListPlaceholder" runat="server" />
+</asp:Content>
+
---
+++ TierList.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using LeagueOfLegends.Model;
+
+public partial class TierList : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        localhost.LeagueOfLegendsWebService webService = new localhost.LeagueOfLegendsWebService();
+        List<localhost.Character> characters = webService.GetCharacters().ToList<localhost.Character>();
+        
+        for (int i = 0; i < characters.Count; i++)
+        {
+            string characterName = characters[i].Name.ToLower();
+            string characterID = characters[i].ID.ToString();
+            int tierPosition;
+            //get tierPosition ... webService.GetTierPosition(characterID);
+
+            string imagePath = "<img src=\"Images/" + characterName + ".png\"></img>";
+        }
+
+    }
+}
