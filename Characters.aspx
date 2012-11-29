70dcaea8e1117088ceac7319d49727de72c62c2a
Daniel Dzanko <danieldzanko@gmail.com>
2012-11-29 02:25:51 -0500

    Added Characters page

---
+++ Characters.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Characters.aspx.cs" Inherits="Characters" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+<style type="text/css">
+    #pageContent_characterContainer
+    {
+        padding:20px;
+        width: 120px;
+        text-align:center;
+        float:left;
+    }
+    #footer
+    {
+        clear:both;
+    }
+    #characters
+    {
+        width:auto;
+    }
+</style>
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="pageContent" Runat="Server">
+<div id="characters">
+<asp:PlaceHolder ID="charactersPlaceholder" runat="server" />
+    </div>
+    <div id="footer"></div>
+</asp:Content>
+
---
+++ Characters.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using localhost;
+
+public partial class Characters : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+		LeagueOfLegendsWebService webservice = new LeagueOfLegendsWebService();
+
+		
+		List<Character> characters = webservice.GetCharacters().ToList<Character>();
+		for (int i = 0; i < characters.Count(); i++)
+		{
+			HtmlGenericControl imageContainer = new HtmlGenericControl("div");
+			HtmlGenericControl nameContainer = new HtmlGenericControl("div");
+			HtmlGenericControl characterContainer = new HtmlGenericControl("div");
+
+			imageContainer.ID = "imageContainer";
+			nameContainer.ID = "nameContainer";
+			characterContainer.ID = "characterContainer";
+			string name = characters[i].Name;
+			string link = "ViewBuilds.aspx?ID=" + characters[i].ID;
+
+			Image charImage = new Image();
+			charImage.ImageUrl = "Images/" + name.ToLower() +".png";
+			HyperLink imageLink = new HyperLink();
+			imageLink.NavigateUrl = link;
+			
+
+			HyperLink nameLink = new HyperLink();
+			nameLink.Text = name;
+			imageLink.Controls.Add(charImage);
+			nameLink.NavigateUrl = link;
+
+			imageContainer.Controls.Add(imageLink);
+			nameContainer.Controls.Add(nameLink);
+			characterContainer.Controls.Add(imageContainer);
+			characterContainer.Controls.Add(nameContainer);
+			charactersPlaceholder.Controls.Add(characterContainer);
+
+		}
+    }
+}
